589e3b42f902b5cfded5d29e138d86e7a29ba422
Damian Szczygieł <d.szczygiel@itmcode.pl>
2021-04-30 14:55:15 +0200

    search|Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import SearchPage from './components/views/SearchPage/SearchPage';
 
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-      </Router>
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={'/'} component={SearchPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
